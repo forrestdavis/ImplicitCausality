ad697dae6122a03a2db2ff089c18ecf4c6713369
forrestdavis <fd252@cornell.edu>
2020-07-17 14:29:50 -0400

    Updates to figures again.

--- stats.R
+++ stats.R
@@ -77,8 +77,11 @@ texreg(rt_gpt_model)
 # Reading Time Surp Plots
 #########################
 
+rt_surp$hasIC <- factor(rt_surp$hasIC)
+rt_surp$isHIGH <- factor(rt_surp$isHIGH)
+
 # LSTM
-lstm_rt <- ggplot(rt_surp, aes(x=factor(hasIC), y=LSTM_avg_surp, fill=factor(isHIGH))) +
+lstm_rt <- ggplot(rt_surp, aes(x=hasIC, y=LSTM_avg_surp, fill=isHIGH)) +
   geom_boxplot(notch=TRUE, outlier.size = 0.1) + ylim(0, 11)+ theme(text = element_text(size=18)) + 
   labs(x ="Verb Type", y = "LSTM Surprisal (RC verb)") + 
   scale_x_discrete(breaks=c("0","1"),
@@ -90,7 +93,7 @@ legend <- get_legend(lstm_rt)
 lstm_rt <- lstm_rt + theme(legend.position='none')
 
 # GPT-2 XL
-gpt_rt <- ggplot(rt_surp, aes(x=factor(hasIC), y=gpt_surp, fill=factor(isHIGH))) +
+gpt_rt <- ggplot(rt_surp, aes(x=hasIC, y=gpt_surp, fill=isHIGH)) +
   geom_boxplot(notch=TRUE, outlier.size = 0.1) + ylim(0, 11)+ theme(text = element_text(size=18)) + 
   labs(x ="Verb Type", y = "GPT-2 XL Surprisal (RC verb)") + 
   scale_x_discrete(breaks=c("0","1"),
@@ -98,13 +101,41 @@ gpt_rt <- ggplot(rt_surp, aes(x=factor(hasIC), y=gpt_surp, fill=factor(isHIGH)))
   scale_fill_manual(values = c("#999999", "white"), name= "Type", labels=c("Low", "High"))
 
 # TF-XL
-tf_rt <- ggplot(rt_surp, aes(x=factor(hasIC), y=tf_surp, fill=factor(isHIGH))) +
+tf_rt <- ggplot(rt_surp, aes(x=hasIC, y=tf_surp, fill=isHIGH)) +
   geom_boxplot(notch=TRUE, outlier.size=0.1) + ylim(0, 11)+ theme(text = element_text(size=18)) + 
   labs(x ="Verb Type", y = "TF-XL Surprisal (RC verb)") + 
   scale_x_discrete(breaks=c("0","1"),
                    labels=c("non-IC", "IC")) + theme(legend.position = 'none') +
   scale_fill_manual(values = c("#999999", "white"), name= "Type", labels=c("Low", "High"))
 
+# Humans
+set.seed(23)
+good = rnorm(40, mean=2, sd=1)
+bad = rnorm(40, mean=4, sd=1)
+LowIC <- data.frame("surp" = bad, 'hasIC' = 1, "isHigh" = 0)
+LownonIC <- data.frame("surp" = good, 'hasIC' = 0, "isHigh" = 0)
+HighIC <- data.frame("surp" = good, 'hasIC' = 1, "isHigh" = 1)
+HighnonIC <- data.frame("surp" = bad, 'hasIC' = 0, "isHigh" = 1)
+
+human <- rbind(LowIC, LownonIC, HighIC, HighnonIC)
+human$isHigh <- factor(human$isHigh)
+human$hasIC <- factor(human$hasIC)
+
+human_plot <- ggplot(human, aes(x=hasIC, y=surp, fill=isHigh)) +
+  geom_boxplot(notch=TRUE, outlier.size = 0.1, outlier.shape = NA) + ylim(0, 12)+ ylim(0, 11)+ theme(text = element_text(size=18)) + 
+  labs(x ="Verb Type", y = "Predicted Human Effect") + 
+  scale_x_discrete(breaks=c("0","1"),
+                   labels=c("non-IC", "IC")) + 
+  scale_fill_manual(values = c("#999999", "white"), name= "Type", labels=c("Low", "High"))
+
+human_plot <- human_plot + theme(axis.text.y=element_blank(), axis.ticks.y=element_blank())
+
+human_plot <- human_plot  + theme(legend.text = element_text(size=30),
+                                  legend.title = element_text(size=30),
+                                  legend.position = c(0.02, 0.98), 
+                                  legend.justification = c("left", "top"), 
+                                  legend.box.just = "center", 
+                                  legend.margin = margin(6, 6, 6, 6))
 plot_row <- plot_grid(lstm_rt, tf_rt)
 
 title <- ggdraw() + 
@@ -129,9 +160,10 @@ plot_row <- plot_grid(
 #Put them together 
 rt_plots <- ggdraw() + draw_plot(plot_row, x=0, y= 0.5, width=1, height=0.5) +
   draw_plot(gpt_rt, x=0, y= 0, width=0.5, height=0.5) + 
-  draw_plot(legend, x=0.5, y= 0, width=0.5, height=0.5) + 
-  draw_plot_label(label = c("c)", "a)", "b)"), size = 15,
-                  x = c(0.05, 0.05, 0.55), y = c(0.07, 0.57, 0.57))
+  draw_plot(human_plot, x=0.5, y= 0, width=0.5, height=0.5) + 
+  draw_plot_label(label = c("c)", "a)", "b)", "d)"), size = 15,
+                  x = c(0.05, 0.05, 0.55, 0.55), y = c(0.07, 0.57, 0.57, 0.07))
+
 
 #########################
 # Reading Time SIM Stats
@@ -250,6 +282,37 @@ rt_who_plots <- ggdraw() + draw_plot(plot_row, x=0, y= 0.5, width=1, height=0.5)
   draw_plot_label(label = c("c)", "a)", "b)", "d)"), size = 15,
                   x = c(0.03, 0.03, 0.35, 0.79), y = c(0.07, 0.57, 0.57, 0.07))
 
+
+#GPT-2 XL who big
+gpt_who_sim$hasIC <- factor(gpt_who_sim$hasIC)
+gpt_who_sim <- subset(gpt_who_sim, num=='pl')
+gpt_who_sim$NP <- factor(gpt_who_sim$NP)
+
+gpt_who <- ggplot(gpt_who_sim, aes(x=factor(layer), y=sim, fill=interaction(NP, hasIC))) +
+  geom_boxplot(notch=TRUE, outlier.size=0.1) + ylim(0, 1) + theme(text = element_text(size=12)) + theme(legend.position='none') + 
+  labs(x ="Hidden Layer", y = "GPT-2 XL Similarity") + scale_fill_manual(values = c("#9999CC", "gold3", "darkorchid3", "darkgoldenrod4"), name= "Noun+Verb", labels=c("High+NonIC", "Low+NonIC", "High+IC", "Low+IC"))
+
+plot_row <- plot_grid(gpt_who, human_plot, rel_widths =c(2.5, 0.5))
+
+title <- ggdraw() + 
+  draw_label(
+    "Relative Clause Attachment with Implicit Causality who Similarity",
+    fontface = 'bold',
+    x = 0,
+    hjust = 0
+  ) +
+  theme(
+    # add margin on the left of the drawing canvas,
+    # so title is aligned with left edge of first plot
+    plot.margin = margin(0, 0, 0, 7)
+  )
+plot_row <- plot_grid(
+  title, plot_row,
+  ncol = 1,
+  # rel_heights values control vertical title margins
+  rel_heights = c(0.1, 1.2)
+)
+
 ###########
 #RT were/was
 ############
@@ -346,50 +409,96 @@ bad = rnorm(40, mean=1, sd=1)
 
  
 #GPT-2 XL were
-gpt_were_HIGH <- subset(gpt_were_sim, isHIGH==1)
-gpt_were_LOW <- subset(gpt_were_sim, isHIGH==0)
-
-gpt_were_HIGH_small <- subset(gpt_were_HIGH, layer %% 3 == 0)
-gpt_were_LOW_small <- subset(gpt_were_LOW, layer %% 3 ==0)
-
-gpt_were_h <- ggplot(gpt_were_HIGH_small, aes(x=factor(layer), y=sim, fill=interaction(NP, hasIC))) +
-geom_boxplot(notch=TRUE, outlier.size=0.1)  + ylim(-0.1, 1)+ theme(text = element_text(size=12))  + 
-labs(x ="Hidden Layer", y = "GPT-2 XL Similarity with High Agree") + scale_fill_manual(values = c("#9999CC", "gold3", "darkorchid3", "darkgoldenrod4"), name= "Noun+Verb", labels=c("High+NonIC", "Low+NonIC", "High+IC", "Low+IC"))
-
-gpt_were_h <- gpt_were_h  + theme(legend.position = c(0.01, 0.01), 
-                              legend.justification = c("left", "bottom"), 
-                              legend.box.just = "right", 
-                              legend.margin = margin(6, 6, 6, 6))
-
-
-gpt_were_l <- ggplot(gpt_were_LOW_small, aes(x=factor(layer), y=sim, fill=interaction(NP, hasIC))) +
-geom_boxplot(notch=TRUE, outlier.size=0.1)  + ylim(-0.1, 1)+ theme(text = element_text(size=12))  + theme(legend.position = 'none') + 
-labs(x ="Hidden Layer", y = "GPT-2 XL Similarity with Low Agree") + scale_fill_manual(values = c("#9999CC", "gold3", "darkorchid3", "darkgoldenrod4"), name= "Noun+Verb", labels=c("High+NonIC", "Low+NonIC", "High+IC", "Low+IC"))
-
-
-gpt_were_plots <- plot_grid(gpt_were_h, gpt_were_l, ncol=1, labels = c('a)', 'b)'), 
-                         label_size = 12,
-                         label_x = 0.03, label_y = 0.01,
-                         hjust = -0.5, vjust = -0.5)
-
-title <- ggdraw() + 
-draw_label(
-  "Relative Clause Attachment with Implicit Causality RC Verb Similarity",
-  fontface = 'bold',
-  x = 0,
-  hjust = 0
-) +
-theme(
-  # add margin on the left of the drawing canvas,
-  # so title is aligned with left edge of first plot
-  plot.margin = margin(0, 0, 0, 7)
-)
-gpt_were_plots <- plot_grid(
-title, gpt_were_plots,
-ncol = 1,
-# rel_heights values control vertical title margins
-rel_heights = c(0.1, 1.2)
-) 
+  gpt_were_HIGH <- subset(gpt_were_sim, isHIGH==1)
+  gpt_were_LOW <- subset(gpt_were_sim, isHIGH==0)
+  
+  gpt_were_h <- ggplot(gpt_were_HIGH, aes(x=factor(layer), y=sim, fill=interaction(NP, hasIC))) +
+    geom_boxplot(notch=TRUE, outlier.size = 0.1)  + ylim(-0.1, 1)+ theme(text = element_text(size=12))  + 
+    labs(x ="Hidden Layer", y = "GPT-2 XL Similarity with High Agree") + scale_fill_manual(values = c("#9999CC", "gold3", "darkorchid3", "darkgoldenrod4"), name= "Noun+Verb", labels=c("High+NonIC", "Low+NonIC", "High+IC", "Low+IC"))
+  
+  gpt_were_h <- gpt_were_h  + theme(legend.position = c(0.05, 0.05), 
+                                  legend.justification = c("left", "bottom"), 
+                                  legend.box.just = "right", 
+                                  legend.margin = margin(6, 6, 6, 6))
+  
+  
+  gpt_were_l <- ggplot(gpt_were_LOW, aes(x=factor(layer), y=sim, fill=interaction(NP, hasIC))) +
+    geom_boxplot(notch=TRUE, outlier.size = 0.1)  + ylim(-0.1, 1)+ theme(text = element_text(size=12))  + theme(legend.position = 'none') + 
+    labs(x ="Hidden Layer", y = "GPT-2 XL Similarity with Low Agree") + scale_fill_manual(values = c("#9999CC", "gold3", "darkorchid3", "darkgoldenrod4"), name= "Noun+Verb", labels=c("High+NonIC", "Low+NonIC", "High+IC", "Low+IC"))
+  
+  set.seed(42)
+  best = rnorm(40, mean=6, sd=1)
+  good = rnorm(40, mean=4, sd=1)
+  ok = rnorm(40, mean=2.5, sd=1)
+  bad = rnorm(40, mean=1, sd=1)
+  HighnonIC <- data.frame("sim" = good, "NP" = 1, 'hasIC' = 0)
+  LownonIC <- data.frame("sim" = ok, "NP" = 2, 'hasIC' = 0)
+  HighIC <- data.frame("sim" = best, "NP" = 1, 'hasIC' = 1)
+  LowIC <- data.frame("sim" = bad, "NP" = 2, 'hasIC' = 1)
+  
+  human <- rbind(HighnonIC, LownonIC, HighIC, LowIC)
+  human$hasIC <- factor(human$hasIC)
+  human$NP <- factor(human$NP)
+  
+  human_H_plot <- ggplot(human, aes(y=sim, fill=interaction(NP, hasIC))) +
+    geom_boxplot(notch=TRUE, outlier.size=0.1, outlier.shape=NA) + ylim(0, 8) + theme(text = element_text(size=12)) +
+    labs(x ="Hidden Layer", y = "Predicted Human Effect") + 
+    scale_fill_manual(values = c("#9999CC", "gold3", "darkorchid3", "darkgoldenrod4"), name= "Noun+Verb", labels=c("High+NonIC", "Low+NonIC", "High+IC", "Low+IC"))
+  
+  human_H_plot <- human_H_plot + theme(axis.text.y=element_blank(), axis.ticks.y=element_blank(), 
+                                       axis.text.x=element_blank(), axis.ticks.x=element_blank()) + theme(legend.position='none')
+  
+  set.seed(42)
+  best = rnorm(40, mean=6, sd=1)
+  good = rnorm(40, mean=4, sd=1)
+  ok = rnorm(40, mean=2.5, sd=1)
+  bad = rnorm(40, mean=1, sd=1)
+  HighnonIC <- data.frame("sim" = bad, "NP" = 1, 'hasIC' = 0)
+  LownonIC <- data.frame("sim" = best, "NP" = 2, 'hasIC' = 0)
+  HighIC <- data.frame("sim" = ok, "NP" = 1, 'hasIC' = 1)
+  LowIC <- data.frame("sim" = good, "NP" = 2, 'hasIC' = 1)
+  
+  human <- rbind(HighnonIC, LownonIC, HighIC, LowIC)
+  human$hasIC <- factor(human$hasIC)
+  human$NP <- factor(human$NP)
+  
+  human_L_plot <- ggplot(human, aes(y=sim, fill=interaction(NP, hasIC))) +
+    geom_boxplot(notch=TRUE, outlier.size=0.1, outlier.shape=NA) + ylim(0, 8) + theme(text = element_text(size=12)) +
+    labs(x ="Hidden Layer", y = "Predicted Human Effect") + 
+    scale_fill_manual(values = c("#9999CC", "gold3", "darkorchid3", "darkgoldenrod4"), name= "Noun+Verb", labels=c("High+NonIC", "Low+NonIC", "High+IC", "Low+IC"))
+  
+  human_L_plot <- human_L_plot + theme(axis.text.y=element_blank(), axis.ticks.y=element_blank(), 
+                                       axis.text.x=element_blank(), axis.ticks.x=element_blank()) + theme(legend.position = 'none')
+  
+  
+  plot_row <- plot_grid(gpt_were_h, human_H_plot, rel_widths =c(2.5, 0.5))
+  plot_bottom_row <- plot_grid(gpt_were_l, human_L_plot, rel_widths=c(2.5, 0.5))
+  
+  title <- ggdraw() + 
+    draw_label(
+      "Relative Clause Attachment with Implicit Causality RC Verb Similarity",
+      fontface = 'bold',
+      x = 0,
+      hjust = 0
+    ) +
+    theme(
+      # add margin on the left of the drawing canvas,
+      # so title is aligned with left edge of first plot
+      plot.margin = margin(0, 0, 0, 7)
+    )
+  
+  plot_row <- plot_grid(
+    title, plot_row,
+    ncol = 1,
+    # rel_heights values control vertical title margins
+    rel_heights = c(0.1, 1.2)
+  )
+  
+  gpt_were_plots <- ggdraw() + draw_plot(plot_row, x=0, y= 0.5, width=1, height=0.5) +
+    draw_plot(plot_bottom_row, x=0, y= 0, width=1, height=0.5) + 
+    draw_plot_label(label = c("c)", "a)", "b)", "d)"), size = 15,
+                    x = c(0.03, 0.03, 0.815, 0.815), y = c(0.07, 0.57, 0.57, 0.07))
+  
 
 
 #########################
@@ -421,17 +530,17 @@ pronoun_surp$hasIC <- as.numeric(pronoun_surp$hasIC)
 lstm_pronoun_model <- lmer(LSTM_avg_surp ~ hasIC*isHigh*gender + (1|item), data=pronoun_surp)
 summary(lstm_pronoun_model)
 anova(lstm_pronoun_model)
-texreg(lstm_pronoun_model)
+texreg(lstm_pronoun_model, digits=4)
 
 tf_pronoun_model <- lmer(tf_surp ~ hasIC*isHigh*gender + (1|item), data=pronoun_surp)
 summary(tf_pronoun_model)
 anova(tf_pronoun_model)
-texreg(tf_pronoun_model)
+texreg(tf_pronoun_model, digits=4)
 
 gpt_pronoun_model <- lmer(gpt_surp ~ hasIC*isHigh*gender + (1|item), data=pronoun_surp)
 summary(gpt_pronoun_model)
 anova(gpt_pronoun_model)
-texreg(gpt_pronoun_model)
+texreg(gpt_pronoun_model, digits=4)
 
 pronoun_HIGH <- subset(pronoun_surp, isHigh == 1)
 pronoun_LOW <- subset(pronoun_surp, isHigh == 0)
@@ -575,7 +684,7 @@ lstm_pronoun_sim$hasIC <- as.numeric(lstm_pronoun_sim$hasIC)
 pronoun_sim_lstm_model <- lmer(sim ~ hasIC*NP*layer*gender + (1|item), data=lstm_pronoun_sim)
 summary(pronoun_sim_lstm_model)
 anova(pronoun_sim_lstm_model)
-texreg(pronoun_sim_lstm_model, digits=3)
+texreg(pronoun_sim_lstm_model, digits=4)
 
 lstm_pronoun_HIGH_IC <- subset(lstm_pronoun_sim, hasIC==1 & layer == 2 & NP == 1)
 lstm_pronoun_HIGH_nonIC <- subset(lstm_pronoun_sim, hasIC==0 & layer == 2 & NP == 1)
@@ -608,10 +717,10 @@ t.test(tf_pronoun_LOW_IC$sim, tf_pronoun_LOW_nonIC$sim)
 gpt_pronoun_sim$hasIC <- gpt_pronoun_sim$bias > 0
 gpt_pronoun_sim$hasIC <- as.numeric(gpt_pronoun_sim$hasIC)
 
-pronoun_sim_gpt_model <- lmer(sim ~ hasIC*NP*layer*gender + (1|item), data=gpt_pronoun_sim)
+pronoun_sim_gpt_model <- lmer(sim ~ bias*NP*layer*gender + (1|item), data=gpt_pronoun_sim)
 summary(pronoun_sim_gpt_model)
 anova(pronoun_sim_gpt_model)
-texreg(pronoun_sim_gpt_model, digits=4)
+texreg(pronoun_sim_gpt_model, digits=8)
 
 
 gpt_pronoun_HIGH_IC <- subset(gpt_pronoun_sim, hasIC==1 & layer == 48 & NP == 1)
@@ -711,3 +820,29 @@ pronoun_plots <- ggdraw() + draw_plot(plot_row, x=0, y= 0.5, width=1, height=0.5
   draw_plot_label(label = c("c)", "a)", "b)", "d)"), size = 15,
                   x = c(0.03, 0.03, 0.35, 0.79), y = c(0.07, 0.57, 0.57, 0.07))
 
+#gpt-big
+gpt_pronoun <- ggplot(gpt_pronoun_sim, aes(x=factor(layer), y=sim, fill=interaction(NP, hasIC))) +
+  geom_boxplot(notch=TRUE, outlier.size=0.1) + ylim(0, 1) + theme(text = element_text(size=12)) + theme(legend.position='none') + 
+  labs(x ="Hidden Layer", y = "GPT-2 XL Similarity") + 
+  scale_fill_manual(values = c("#9999CC", "gold3", "darkorchid3", "darkgoldenrod4"), name= "Noun+Bias", labels=c("Subject+Obj Bias", "Object+Obj Bias", "Subject+Subj Bias", "Object+Subj Bias"))
+
+plot_row <- plot_grid(gpt_pronoun, human_plot, rel_widths =c(2.5, 0.5))
+
+title <- ggdraw() + 
+  draw_label(
+    "Pronoun Reference with Implicit Causality Similarity",
+    fontface = 'bold',
+    x = 0,
+    hjust = 0
+  ) +
+  theme(
+    # add margin on the left of the drawing canvas,
+    # so title is aligned with left edge of first plot
+    plot.margin = margin(0, 0, 0, 7)
+  )
+plot_row <- plot_grid(
+  title, plot_row,
+  ncol = 1,
+  # rel_heights values control vertical title margins
+  rel_heights = c(0.1, 1.2)
+)
